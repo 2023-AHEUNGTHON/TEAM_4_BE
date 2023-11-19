5b443d5c23d5a37c14a488040e3ff79576336ab5
pmsu2007 <pmsu2008@gmail.com>
2023-11-19 16:20:56 +0900

    init project

---
+++ twoneone/src/test/java/aheung/likelion/twoneone/TwoneoneApplicationTests.java
@@ -0,0 +1,13 @@
+package aheung.likelion.twoneone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoneoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
