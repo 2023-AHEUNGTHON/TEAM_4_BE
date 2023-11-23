01bd91255f2f99294f446a17ba399bf77bd8de4d
pmsu2007 <pmsu2008@gmail.com>
2023-11-23 22:17:05 +0900

    feat : 게시글에 대한 좋아요를 생성하는 기능 구현

--- twoneone/src/main/java/aheung/likelion/twoneone/repository/PostLikeRepository.java
+++ twoneone/src/main/java/aheung/likelion/twoneone/repository/PostLikeRepository.java
@@ -1,7 +1,10 @@
 package aheung.likelion.twoneone.repository;
 
+import aheung.likelion.twoneone.domain.community.Post;
 import aheung.likelion.twoneone.domain.community.PostLike;
+import aheung.likelion.twoneone.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    boolean existsByPostAndUser(Post post, User user);
 }
