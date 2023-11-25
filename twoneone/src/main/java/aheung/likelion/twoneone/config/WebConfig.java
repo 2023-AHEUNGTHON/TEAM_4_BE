8f5dd377076f458013fc306498154c0ff852d208
dongyun <0902hdy@gmail.com>
2023-11-25 10:42:12 +0900

    fix cors

--- twoneone/src/main/java/aheung/likelion/twoneone/config/WebConfig.java
+++ twoneone/src/main/java/aheung/likelion/twoneone/config/WebConfig.java
@@ -17,23 +17,23 @@ public class WebConfig implements WebMvcConfigurer {
 //                .allowedOrigins("*") // 모든 출처 허용
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
-                .allowedOriginPatterns("http://*.example.com") // 패턴을 사용하여 오리진 허용
+                .allowedOriginPatterns("*") // 패턴을 사용하여 오리진 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true); // 쿠키를 포함한 요청 허용
     }
 
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.setMaxAge(6000L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
-        filterBean.setOrder(0);
-        return filterBean;
-    }
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(false);
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        config.setMaxAge(6000L);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+//        filterBean.setOrder(0);
+//        return filterBean;
+//    }
 }
