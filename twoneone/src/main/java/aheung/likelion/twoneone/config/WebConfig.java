0a38d85823340ba9840c06fd7e01f6daefd4ca33
dongyun <0902hdy@gmail.com>
2023-11-25 10:24:31 +0900

    delete test code

--- twoneone/src/main/java/aheung/likelion/twoneone/config/WebConfig.java
+++ twoneone/src/main/java/aheung/likelion/twoneone/config/WebConfig.java
@@ -1,6 +1,11 @@
 package aheung.likelion.twoneone.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +20,20 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("http://*.example.com") // 패턴을 사용하여 오리진 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true); // 쿠키를 포함한 요청 허용
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(6000L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+        return filterBean;
     }
 }
